ac402eb374ef11ea467ec73d24bba69c2368e0ed
Hahaha95277 <108458445+Hahaha95277@users.noreply.github.com>
2025-05-18 18:41:44 +0800

    Update main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-import App from "./App.js";
-
-const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(React.createElement(App));
